294221f3b3b33a614cc56cdc2363c11b4c2b2015
luis <luis@luis-W240EU-W250EUQ-W270EUQ>
2015-01-25 23:32:22 -0300

    change details

--- application/views/bootstrap/footer.php
+++ application/views/bootstrap/footer.php
@@ -1,3 +1,3 @@
   <footer>
-                <p>&copy; Company 2014</p>
+                <p>&copy; Footer 2014</p>
             </footer>
--- application/views/bootstrap/sidebar.php
+++ application/views/bootstrap/sidebar.php
@@ -2,13 +2,5 @@
     <div class="list-group">
         <a href="#" class="list-group-item active">Link</a>
         <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
     </div>
 </div><!--/.sidebar-offcanvas-->
